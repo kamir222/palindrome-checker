f2df361672a7366bd12da64911f568c717a0cadc
kamir222 <katia.amir@gmail.com>
2017-06-04 12:10:01 +0000

    working code!

--- palindrome-checker.js
+++ palindrome-checker.js
@@ -1,18 +1,23 @@
 function isPalindrome(str) {
+  var str = str.toLowerCase();
   var noSpaces = str.split(" ").join("");
   var mid = Math.floor(noSpaces.length/2);
   var last = noSpaces.length - 1;
+  for (var i = 0; i < mid + 1; i++) {
+    if (noSpaces[i] !== noSpaces[last - i]) {
+    	return false;
+    }
 
-  for (var i = 0; i < mid; i++) {
-    if (str[i] !== str[last - i]) return false;
   }
+  return true;
 }
 
-// Test driver code. These should all log true.
-console.log(isPalindrome('p') === true);
-console.log(isPalindrome('foo') === false);
-console.log(isPalindrome('racecar') === true);
-console.log(isPalindrome('Kayak') === true);
-console.log(isPalindrome('a santa at NASA') === true);
-console.log(isPalindrome('live without evil') === false);
-console.log(isPalindrome('just some random words') === false);
+
+//Test driver code. These should all log true.
+console.log(isPalindrome('p') === true); //false
+console.log(isPalindrome('foo') === false); //true
+console.log(isPalindrome('racecar') === true); //false
+console.log(isPalindrome('Kayak') === true); //false
+console.log(isPalindrome('a santa at NASA') === true); //false
+console.log(isPalindrome('live without evil') === false); //true
+console.log(isPalindrome('just some random words') === false); // true
